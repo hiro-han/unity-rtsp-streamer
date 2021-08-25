166f29ded80e37c3cf9f6b42883347e19f92fa6f
Hiro Han <hirofumi.han@gmail.com>
2021-08-26 01:05:59 +0900

    Added flip

--- Assets/Scripts/RtspStreamer.cs
+++ Assets/Scripts/RtspStreamer.cs
@@ -18,28 +18,33 @@ public class RtspStreamer : MonoBehaviour
     public RenderTexture _renderTexture;
     public float _fps = 0.5f;
     private Texture2D _texture;
+    private Texture2D _flipped_texture;
     private Rect _rect;
     private int _dataSize;
     private float _timeElapsed = 0.0f;
+    private int _width;
+    private int _height;
+
 
     void Start()
     {
         Debug.Log("Enter : Start()");
 
-        int width = _renderTexture.width;
-        int height = _renderTexture.height;
+        _width = _renderTexture.width;
+        _height = _renderTexture.height;
         int depth = 4;
-        _dataSize = width * height * depth;
+        _dataSize = _width * _height * depth;
 
-        _texture = new Texture2D(width, height, TextureFormat.BGRA32, false);
-        _rect = new Rect(0, 0, width, height);
+        _texture = new Texture2D(_width, _height, TextureFormat.BGRA32, false);
+        _flipped_texture = new Texture2D(_width, _height, TextureFormat.BGRA32, false);
+        _rect = new Rect(0, 0, _width, _height);
 
-        Debug.Log("Width : " + width.ToString());
-        Debug.Log("Height : " + height.ToString());
+        Debug.Log("Width : " + _width.ToString());
+        Debug.Log("Height : " + _height.ToString());
         Debug.Log("Depth : " + depth.ToString());
         Debug.Log("Size : " + _dataSize.ToString());
 
-        Initialize(2, "/test", width, height, depth);
+        Initialize(2, "/test", _width, _height, depth);
 
         Debug.Log("Exit : Start()");
     }
@@ -55,6 +60,7 @@ public class RtspStreamer : MonoBehaviour
                 _texture.ReadPixels(_rect, 0, 0);
                 _texture.Apply();
                 RenderTexture.active = currentRenderTexture;
+                FlipTexture(_texture);
                 SetImage(GCHandle.Alloc(_texture.GetPixels32(0), GCHandleType.Pinned).AddrOfPinnedObject(), _dataSize);
 
                 Debug.Log("SetImage()");
@@ -64,5 +70,23 @@ public class RtspStreamer : MonoBehaviour
             _timeElapsed = 0.0f;
         }
         Debug.Log("Exit : Update()");
+    }
+
+    void FlipTexture(Texture2D tex)
+    {
+        var pixels = tex.GetPixels();
+
+        Color[] newPixels = new Color[pixels.Length];
+
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+                newPixels[x + y * _width] = pixels[x + (_height - y -1) * _width];
+            }
+        }
+
+        tex.SetPixels(newPixels);
+        tex.Apply();
     }
 }
